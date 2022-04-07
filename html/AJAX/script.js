a1a8cac2390b14ff5098efe80666cf5e8beb43f7
Vincenzo Pugliese <vincenzo.pugliese.1996@gmail.com>
2022-04-07 22:16:19 +0200

    minor changes

--- html/AJAX/script.js
+++ html/AJAX/script.js
@@ -1,38 +1,28 @@
-document.addEventListener('DOMContentLoaded', function(){
 
-    const call = new XMLHttpRequest()
-    call.open("GET", 'https://jsonplaceholder.typicode.com/posts', true)
-
-
-
-})
-
-
-
-
-
-const ajax = (call) => {
-
+const ajax = () => {
+    const call = new XMLHttpRequest();
+    call.open("GET", 'https://jsonplaceholder.typicode.com/posts', true);
     call.onreadystatechange = () => {
 
-        console.log(call.readyState);
-        console.log(call.status);
-        if (call.readystate == 4 && call.status == 200){
-            //document.getElementById("res").innerHTML += call.responseText.replaceAll(",", "<br>");
-            let obj = eval(call.responseText);
-            let res = document.getElementById("res");
-            obj.forEach(element => {
-                console.log('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-                console.log(element.userId);
-                res.innerHTML += "<p><b>userID: </b>" + element.userId +"</p>";
-                res.innerHTML += "<p><b>id: </b>" + id +"</p>";
-                res.innerHTML += "<p><b>title: </b>" + element.title +"</p>";
-                res.innerHTML += "<p><b>body: </b>" + element.body +"</p>";
-            });
+    
+        if (call.readyState === XMLHttpRequest.DONE) {
+            var status = call.status;
+            if (status === 0 || (status >= 200 && status < 400)) {
+                let obj = eval(call.responseText);
+                let res = document.getElementById("res");
+                obj.forEach(element => {
+                    res.innerHTML += "<p><b>userID: </b>" + element.userId + "</p>";
+                    res.innerHTML += "<p><b>id: </b>" + element.id + "</p>";
+                    res.innerHTML += "<p><b>title: </b>" + element.title + "</p>";
+                    res.innerHTML += "<p><b>body: </b>" + element.body + "</p>";
+                });
+            }
+        }
+        else {
+            console.log("error");
         }
     }
     call.send(null);
-
 }
 
 const ajaxFetch = () =>{
