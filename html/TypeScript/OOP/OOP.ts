9e6ecdf0d32d969e5a4162677383e6636985f5ef
Vincenzo Pugliese <vincenzo.pugliese.1996@gmail.com>
2022-04-08 15:48:37 +0200

    Generics in TypeScript

---
+++ html/TypeScript/OOP/OOP.ts
@@ -0,0 +1,66 @@
+
+interface IRunnable extends IThread{
+    run(): IUser;
+}
+
+interface IUser{
+    username: string;
+    password: string;
+    email: string;
+    enabled?: boolean;
+
+}
+
+interface IThread{
+    start(): void;
+}
+
+abstract class IRunner{
+    
+    name: string;
+    
+    constructor(name: string){
+        this.name = name;
+
+    }
+
+    abstract print():void;
+    abstract set():void;
+}
+
+class IRunnerImpl extends IRunner{
+    
+    name: string;
+
+    constructor(name: string){
+        super(name);
+    }
+    
+    
+    print(): void {
+        throw new Error("Method not implemented.");
+    }
+    set(): void {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+
+interface IPrinterOut<U, V, T, S extends IRunnable >{
+    print(param : U, param1 : V): void;
+    start(param1 : U, param2?: V, param3?:T) : S;
+}
+
+class PrinterOut implements IPrinterOut<Object, {}, number, IRunnable>{
+    print(param: Object, param1: {}): void {
+        throw new Error("Method not implemented.");
+    }
+    start(param1: Object, param2?: {}, param3?: number): IRunnable {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+
+type Obj = IUser;
+
+const t: Obj = null;
